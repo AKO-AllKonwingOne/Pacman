5ae68cd4aec85a5dc79a3a20068eab7e32c3c2cf
AKO-AllKonwingOne <hugo.lindgren.svensson@gmail.com>
2024-04-11 10:28:53 +0200

    gjorde pacman och hans rörelse

--- src/Gamebox.java
+++ src/Gamebox.java
@@ -1,2 +1,2 @@
-package PACKAGE_NAME;public class Gamebox {
+public class Gamebox {
 }
--- src/Pacman.java
+++ src/Pacman.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Pacman {
+public class Pacman {
+    private int row;
+    private int col;
+
+    public Pacman(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public void moveUp() {
+        row--;
+    }
+
+    public void moveDown() {
+        row++;
+    }
+
+    public void moveLeft() {
+        col--;
+    }
+
+    public void moveRight() {
+        col++;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
 }
