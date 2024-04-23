78cd1c5afeb831042b0c166d76618a4d2eaf6fc4
AKO-AllKonwingOne <hugo.lindgren.svensson@gmail.com>
2024-04-23 15:35:57 +0200

    arbetade på ghost.java under fritid.

--- src/Ghost.java
+++ src/Ghost.java
@@ -1,2 +1,19 @@
+import java.util.Random;
+
 public class Ghost {
-}
+    private int row;
+    private int col;
+
+    public Ghost(int row, int col) {
+        this.row = row;
+        this.col = col;
+    }
+
+    public void move(Gamebox gamebox) {
+        Random random = new Random();
+        int[] dx = {0, 0, 1, -1};
+        int[] dy = {1, -1, 0, 0};
+        int direction = random.nextInt(4); // 0: up, 1: down, 2: left, 3: right
+
+        int newRow = row + dx[direction];
+        int newCol = col + dy[direction];
