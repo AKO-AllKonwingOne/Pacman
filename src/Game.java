6a1f6bea7984e42d1e65c2cc60390ebb643138e0
AKO-AllKonwingOne <hugo.lindgren.svensson@gmail.com>
2024-04-25 11:22:33 +0200

    arbeta lite på game.java

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class Game {
-}
+    private final Gamebox gamebox;
+    private final Pacman pacman;
+    private final Ghost ghost;
+
+    public Game() {
+        final int ROWS = 10;
+        final int COLS = 10;
+
+        gamebox = new Gamebox(ROWS, COLS);
+        pacman = new Pacman(ROWS / 2, COLS / 2);
+        ghost = new Ghost(ROWS / 2, COLS / 2);
+
+        play();
+    }
